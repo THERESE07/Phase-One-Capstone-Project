60c20fc481ecd5aa29680a1660587820d21f6395
THERESE07 <niyigena.therese@gmail.com>
2026-02-25 09:49:08 +0200

    Add custom exceptions: StudentAlreadyEnrolledException and CourseFullException

---
+++ src/com/niyigena/university/exceptions/StudentAlreadyEnrolledException.java
@@ -0,0 +1,7 @@
+package com.niyigena.university.exceptions;
+
+public class StudentAlreadyEnrolledException extends Exception {
+    public StudentAlreadyEnrolledException(String message) {
+        super(message);
+    }
+}
